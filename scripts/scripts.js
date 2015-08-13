ffaa372da8170f022afa7d424f282109ff64a803
Felice DeNigris <felideni@gmail.com>
2015-08-12 23:39:19 -0400

    fixing some css with gif

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,45 +1,51 @@
+
+
+
+
+jQuery(document).ready(function($){
+
 //CLICK SERVICES :SET TO ACTIVE
-$(function(){ 
+
   $("#services-li").click(function(){
     $("#home-li, #work-li, #reachme-li, #photography-li").removeClass("active");
     $("#services-li").addClass("active");
   });
-});
+
 
 
 //CLICK WORK :SET TO ACTIVE
-$(function(){ 
+
   $("#work-li").click(function(){
     $("#home-li, #services-li, #reachme-li, #photography-li").removeClass("active");
     $("#work-li").addClass("active");
   });
-});
+
 
 
 //CLICK REACH ME :SET TO ACTIVE
-$(function(){ 
+
   $("#reachme-li").click(function(){
     $("#home-li, #services-li, #work-li,#photography-li").removeClass("active");
     $("#reachme-li").addClass("active");
   });
-});
 
-$(function(){ 
+//CLICK HOME:SET TO ACTIVE
+
   $("#home-li").click(function(){
     $("#reachme-li, #services-li, #work-li, #photography-li").removeClass("active");
     $("#home-li").addClass("active");
   });
-});
 
-$(function(){ 
+//CLICK PHOTOGRAPHY :SET TO ACTIVE
+
   $("#photography-li").click(function(){
     $("#reachme-li, #services-li, #work-li, #home-li").removeClass("active");
     $("#photography-li").addClass("active");
   });
-});
 
 
-$(function(){
+//HOVER ON GIF
+
   $('.bg').hover(function(){
     $(this).attr("src", "../images/Intro_2k.gif");
     console.log("GIF Hover");
@@ -47,14 +53,25 @@ $(function(){
   function(){
     $(this).attr("src", "../images/Intro_2k.gif");
   });
-});
+
 
 
 //SCROLL TO SERVICES: SET TO ACTIVE
 
-/*$('photography-li').waypoints('sticky',{
-  direction:'up',
-  handler: function(direction) {
-    console.log('Scrolled to waypoint!');
-  }
-});*/
+  $('#photography-li').waypoint(function(event, direction){
+
+    if(direction === "down"){
+      $("#reachme-li, #services-li, #work-li, #home-li").removeClass("active");
+      $("#photography-li").addClass("active");
+    }
+    else{
+      $("#reachme-li, #services-li, #work-li, #home-li").removeClass("active");
+      $("#photography-li").addClass("active");
+    }
+  });
+});
+
+
+
+
+
