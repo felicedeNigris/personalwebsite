fae68a1d323187810c148d4a576a93e25d0c7186
Felice DeNigris <felideni@gmail.com>
2015-08-18 22:20:14 -0400

    Add NodeJS,serverJs,Procfile

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname));
+
+var server = app.listen(app.get('port'));
