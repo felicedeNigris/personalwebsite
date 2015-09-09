167f092491d4ea714335d9aaccaa4c5ef2ea8310
Felice DeNigris <felideni@gmail.com>
2015-09-08 20:24:07 -0400

    Added Web Dev work photo gallery and github social media icon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -253,14 +253,14 @@ $("#meNav").click(function(){
 
 //// MODAL PHOTO FEATURE
 
-$('.img-responsive').on('click',function(){
+$(".img-responsive").on('click',function(){
   var src = $(this).attr('src');
   var img = '<img src="' + src + '" class="img-responsive"/>';
   //Start of new code
   var index = $(this).parent('li').index();
   var html = '';
   html += img;
-  html += '<div style="height:25px;clear:both;display:block;">';
+  html += '<div style="height:25px;clear:both;display:block; -webkit-user-select:none;-webkit-touch-callout:none;">';
   html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
   html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
   html += '</div>';
